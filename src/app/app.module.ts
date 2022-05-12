16563c3e30c2b2c855a9e5bafab3798e688b98f1
mfrugoni <miguelmfrugoni@gmail.com>
2022-05-12 18:21:26 -0300

    Agregar proyecto Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { AboutComponent } from './componentes/about/about.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    AboutComponent,
+    PerfilComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    ContactoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
